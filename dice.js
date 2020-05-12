1bf9829284c5d8d4f23d78651bb806c8dc4ab88b
Gopi Chand <gopichand7n3@gmail.com>
2020-05-12 17:57:38 +0530

    add short version

--- dice.js
+++ dice.js
@@ -1,3 +1,5 @@
+//***********1st version*************
+
 var firstImage = document.querySelector(".img1");
 var secondImage = document.querySelector(".img2");
 var H1 = document.querySelector("h1");
@@ -44,3 +46,23 @@ if(firstRandomNumber < secondRandomNumber){
 } else {
   H1.innerHTML = "<h1>Oops! Its a Tie.</h1>";
 }
+
+//************2nd version (Short Method)*************
+
+// var firstRandomNumber = Math.floor(Math.random() * 6) + 1;
+// var firstRandomImage = "images/dice" + firstRandomNumber + ".png";
+// var firstImage = document.querySelector(".img1");
+// firstImage.setAttribute("src", firstRandomImage);
+//
+// var secondRandomNumber = Math.floor(Math.random() * 6) + 1;
+// var secondRandomImage = "images/dice" + secondRandomNumber + ".png";
+// var secondImage = document.querySelector(".img2");
+// secondImage.setAttribute("src", secondRandomImage);
+//
+// if(firstRandomNumber < secondRandomNumber){
+//   document.querySelector("h1").innerHTML = "<h1>Player 2 Wins!  <img src='images/flag.png' style = 'width: 10%'></h1>";
+// } else if(firstRandomNumber > secondRandomNumber){
+//   document.querySelector("h1").innerHTML = "<h1><img src='images/flag.png' style = 'width: 10%'> Player 1 Wins!</h1>";
+// } else {
+//   document.querySelector("h1").innerHTML = "<h1>Draw!</h1>";
+// }
